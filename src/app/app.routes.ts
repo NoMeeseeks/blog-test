b498bb9b709ffa69eb8506d282b82eb7c6abc552
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-08 21:55:31 -0500

    creacion de ruta y carga perezosa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        loadChildren: (() => import('./landing-page/landing-page.routes').then(m => m.routes))
+    }
+
+];
