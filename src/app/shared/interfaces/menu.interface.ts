7d32d0d7b3e0a8a1ae10bbb98fa902b12bcff738
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-10 19:44:23 -0500

    personalizacion del componente de menu aun falta agregar las rutas

---
+++ src/app/shared/interfaces/menu.interface.ts
@@ -0,0 +1,4 @@
+export interface Menu {
+    titulo: string,
+    ruta: string,
+}
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Menu } from '../interfaces/menu.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'shared-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-
+  public listaMenu: Menu[] = [
+    {
+      titulo: 'Sobre mi',
+      ruta: ''
+    },
+    {
+      titulo: 'Posts recientes',
+      ruta: ''
+    },
+    {
+      titulo: 'Sigueme',
+      ruta: ''
+    },
+    {
+      titulo: 'Categorias',
+      ruta: ''
+    }
+  ]
 }
