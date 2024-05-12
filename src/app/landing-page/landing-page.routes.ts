09e05e058e28a4d0da589a6fac099ef47b0d4836
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-11 19:37:53 -0500

    actualizacion de las rutas

--- src/app/landing-page/landing-page.routes.ts
+++ src/app/landing-page/landing-page.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'main',
         component: CardComponent
+    },
+    {
+        path: 'blog',
+        component: BlogComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'main'
     }
+
 
 ];
