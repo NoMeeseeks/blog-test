eb5b0a33f96b8c3859b789a800195ecadaea8526
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-13 21:42:23 -0500

    solucion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,17 @@ import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'landing',
         component: LayoutComponent,
         loadChildren: (() => import('./landing-page/landing-page.routes').then(m => m.routes))
+    },
+    {
+        path: 'auth',
+        loadChildren: (() => import('./admin/admin.routes').then(m => m.routes))
+    },
+    {
+        path: 'admin',
+        loadChildren: (() => import('./auth/auth.routes').then(m => m.routes))
     }
 
 ];
