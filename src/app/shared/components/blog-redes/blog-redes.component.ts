8b294e14e5f299e32ce894bbf7c55f7048e19a73
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-18 23:05:51 -0500

    componente de redes sociales

---
+++ src/app/shared/components/blog-redes/blog-redes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-redes',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-redes.component.html',
+  styles: ``
+})
+export class BlogRedesComponent {
+
+}
