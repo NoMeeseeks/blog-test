b971453e0468685d2f3c8aeec8f29ba58d9397d0
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-28 19:59:59 -0500

    Creacion Visual de las paginas de authenticacion, creacion del servicio de auth y ajustes de sus rutas

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { VerificationPageComponent } from './pages/verification-page/verification-page.component';
 
 
 export const routes: Routes = [
@@ -16,6 +17,10 @@ export const routes: Routes = [
     {
         path: 'reset',
         component: ResetPasswordComponent
+    },
+    {
+        path: 'verification/:correo',
+        component: VerificationPageComponent
     },
     {
         path: '**',
